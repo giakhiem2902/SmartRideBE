e4b871a2f92a9813cc3c56fe0d5b092ed705e874
giakhiem2902 <giakhiemltgk2902@gmail.com>
2026-01-14 15:52:36 +0700

    Final

---
+++ Migrations/20260114080422_AddAdminChatSystem.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartRideBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminChatSystem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdminChatRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    AdminId = table.Column<int>(type: "int", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminChatRooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdminChatRooms_AspNetUsers_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AdminChatRooms_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminChatMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdminChatRoomId = table.Column<int>(type: "int", nullable: false),
+                    SenderId = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminChatMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdminChatMessages_AdminChatRooms_AdminChatRoomId",
+                        column: x => x.AdminChatRoomId,
+                        principalTable: "AdminChatRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AdminChatMessages_AspNetUsers_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminChatMessages_AdminChatRoomId",
+                table: "AdminChatMessages",
+                column: "AdminChatRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminChatMessages_SenderId",
+                table: "AdminChatMessages",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminChatRooms_AdminId",
+                table: "AdminChatRooms",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminChatRooms_UserId",
+                table: "AdminChatRooms",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminChatMessages");
+
+            migrationBuilder.DropTable(
+                name: "AdminChatRooms");
+        }
+    }
+}
--- obj/Debug/net9.0/SmartRideBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/SmartRideBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartRideBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba5fe568c593151ddccaa2ed971d356809f5441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e44f381c0fd9be09895fb82972708f15dfa1e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartRideBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartRideBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
